69233eafa8f153f69431281ea755bcaba74ae6b7
YangNingBo <17538137290@163.com>
2019-02-22 11:52:38 +0800

    单纯commit

--- src/main/java/com/haku/DemoApplication.java
+++ src/main/java/com/haku/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     //我在IDEA添加了注释哟
     //这次我就单纯的把commit后的版本库push到远程库
+    //单纯commit 然后push
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
